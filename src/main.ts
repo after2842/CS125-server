06a9450db26f2233303619a9009b3d97124ab615
Samuel <after2842@gmail.com>
2026-02-04 17:14:55 -0800

    modified main.ts file to distinguish host/port for dev and prod

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  const port = process.env.PORT ? Number(process.env.PORT) : 3001;
+  const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
+
+  await app.listen(port, host);
 }
 bootstrap();
